b3776790073d8b107e40e6ba713faf7c5a74070b
Yasser Qureshi <49324530+yasserqureshi1@users.noreply.github.com>
2020-04-14 22:33:41 +0100

    Update covid19_dashboard.py

--- covid19_dashboard.py
+++ covid19_dashboard.py
@@ -3,40 +3,69 @@ import dash_html_components as html
 import dash
 from dash.dependencies import Input, Output
 import pandas as pd
+import plotly.graph_objects as go
 
-app = dash.Dash()
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 df1 = pd.read_json('https://coronavirus-tracker-api.herokuapp.com/confirmed')
 df2 = pd.read_json('https://coronavirus-tracker-api.herokuapp.com/deaths')
 df3 = pd.read_json('https://coronavirus-tracker-api.herokuapp.com/recovered')
 
-no_confirmed_ww = df1.latest[1]
-no_deaths_ww = df2.latest[1]
-no_recovered_ww = df3.latest[1]
-
 app.layout = html.Div(
     [
-        html.H1(children='COVID19 Dashboard'),
-        html.H4(children='Worldwide Cases'),
-        html.P(children=['Number of confirmed cases: ', no_confirmed_ww]),
-        html.P(children=['Number of deaths cases: ', no_deaths_ww]),
-        html.P(children=['Number of recovered cases: ', no_recovered_ww]),
-        dcc.Graph(
-            id='Worldwide Cases',
-            figure={
-                'data': [
-                    {'x': ['Confirmed', 'Deaths', 'Recovered'], 'y': [no_confirmed_ww, no_deaths_ww, no_recovered_ww], 'type': 'bar'},
-                ]
-            }
+
+        html.Div([
+            html.H1(children='COVID19 Dashboard', style={'textAlign': 'center', 'font-family': 'Helvetica'}),
+            html.H4(children='Worldwide Cases', style={'textAlign': 'center', 'font-family': 'Helvetica'}),
+        ], className="row", ),
+
+        html.Div([
+
+            html.Div(
+                dcc.Graph(
+                    id='Worldwide Cases',
+                    figure={
+                        'data': [
+                            {'x': ['Confirmed', 'Deaths', 'Recovered'],
+                             'y': [df1.latest[1], df2.latest[1], df3.latest[1]],
+                             'type': 'bar'},
+                        ],
+                    },
+                    style={'height': 400, 'width': 400}
+                ),
+                style={'display': 'inline-block', 'height': '200px'}
+            ),
+
+            html.Div([
+                html.P(children=['Number of confirmed cases: ', df1.latest[1]], style={'font-family': 'Helvetica'}),
+                html.P(children=['Number of deaths cases: ', df2.latest[1]], style={'font-family': 'Helvetica'}),
+                html.P(children=['Number of recovered cases: ', df3.latest[1]], style={'font-family': 'Helvetica'}),
+            ], style={'display': 'inline-block', 'height': '200px'}),
+
+        ], className='row'),
+
+        html.Div(
+            html.H4(children='Plot of Cases in Country', style={'textAlign': 'center', 'font-family': 'Helvetica'}),
+            className='row'),
+
+        html.Div(
+            dcc.Input(
+                id='confirmed',
+                type='text',
+                placeholder='Country',
+            ), className='row', style={'textAlign': 'center'}
         ),
-        html.H4(children='Plot of Cases in Country'),
-        dcc.Input(
-            id='confirmed',
-            type='text',
-            placeholder='Country',
-        ),
-        html.Div(id='output1')
+
+        html.Div([
+            html.Div(id='output1', style={'width': 600, 'height': 400}, className='six columns'),
+            html.Div(id='output2', style={'width': 600, 'height': 400}, className='six columns')],
+            className='row'
+        )
+
     ]
 )
+
 
 @app.callback(
     Output("output1", "children"),
@@ -115,8 +144,57 @@ def update_output(country):
                              {'x': x1, 'y': y1, 'type': 'line', 'name': 'Confirmed'},
                              {'x': x2, 'y': y2, 'type': 'line', 'name': 'Deaths'},
                              {'x': x3, 'y': y3, 'type': 'line', 'name': 'Recovered'}
-                         ]
-                     }
+                         ],
+                         'layout': {
+                             'title': 'Plot of Cases'
+                         }
+                     },
+                     )
+
+
+@app.callback(
+    Output("output2", 'children'),
+    [Input('confirmed', 'value')]
+)
+def table2(country):
+    for i in range(len(df1)):
+        if country == '':
+            xrate = [1, 1]
+            rate = 1
+            break
+        if df1.loc[i, 'locations']['province'] == country:
+            a = df1.loc[i, 'locations']['history']
+            a = sorted(a.items(), key=lambda p: p[1], reverse=False)
+            x1: object
+            x1, y1 = zip(*a)
+            rate = []
+            for j in range(len(y1) - 1):
+                rate.append(int(y1[j + 1]) - int(y1[j]))
+            xrate = x1[1:]
+            break
+        elif df1.loc[i, 'locations']['country'] == country:
+            if df1.loc[i, 'locations']['province'] == '':
+                a = df1.loc[i, 'locations']['history']
+                a = sorted(a.items(), key=lambda p: p[1], reverse=False)
+                x1: object
+                x1, y1 = zip(*a)
+                rate = []
+                for j in range(len(y1) - 1):
+                    rate.append(int(y1[j + 1]) - int(y1[j]))
+                xrate = x1[1:]
+                break
+        else:
+            rate = 1
+            xrate = [1, 1]
+    return dcc.Graph(id=f"{country} Rate",
+                     figure={
+                         'data': [
+                             {'x': xrate, 'y': rate, 'type': 'line', 'name': 'Rate'}
+                         ],
+                         'layout': {
+                             'title': 'Rate of Cases'
+                         }
+                     },
                      )
 
 
