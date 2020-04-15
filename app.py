3f155c599e55cf1c009e5d97e417665b71ce3516
Yasser Qureshi <49324530+yasserqureshi1@users.noreply.github.com>
2020-04-15 01:19:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import plotly.graph_objects as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 df1 = pd.read_json('https://coronavirus-tracker-api.herokuapp.com/confirmed')
 df2 = pd.read_json('https://coronavirus-tracker-api.herokuapp.com/deaths')
